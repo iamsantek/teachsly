c7ae618fa9b038a22d4da3fb615f846f4b3db4d3
Santiago Gonzalez Rojas <santiago@ensolvers.com>
2021-12-13 17:12:19 -0300

    Add keys to dictionary

--- src/dictionaries/dictionary.ts
+++ src/dictionaries/dictionary.ts
@@ -3,16 +3,36 @@ export const translations: { [key: string]: { [key: string]: string } } = {
     CREATE_STUDENT_BUTTON: "Crear estudiante",
     NAME: "Nombre completo",
     CANCEL: "Cancelar",
-    PROCESSING: "Procesando...",
+    PROCESSING: "🕑  Traba",
     PASSWORD: "Contraseña",
     EMAIL: "Email",
+    COURSES: "Cursos",
+    STUDENT_CREATED_MESSAGE:
+      "🚀 Genial! El alumno se cargo exitosamente a la plataforma.",
+    COURSE_CREATED_MESSAGE:
+      "🚀 Excelente! Se agregó correctamente un nuevo curso a la platforma.",
+    COURSE_NAME: "Nombre del curso",
+    COURSE_DATES: "Dias del curso",
+    COURSE_SCHEDULE: "Horarios del curso",
+    CREATE_COURSE_BUTTON: "Crear Curso",
+    CREATE_COURSE_MODAL_TITLE: "Creación de Curso",
   },
   en: {
     CREATE_STUDENT_BUTTON: "Create student",
     NAME: "Full name",
     CANCEL: "Cancel",
-    PROCESSING: "Processing...",
+    PROCESSING: "🕑 Working on it",
     PASSWORD: "Password",
     EMAIL: "Email",
+    COURSES: "Courses",
+    STUDENT_CREATED_MESSAGE:
+      "🚀 Great! The Student has been successfully created.",
+    COURSE_CREATED_MESSAGE:
+      "🚀 Fantastic! A new course has been added to the platform.",
+    COURSE_NAME: "Course name",
+    COURSE_DATES: "Course days",
+    COURSE_SCHEDULE: "Timetable",
+    CREATE_COURSE_BUTTON: "Create Course",
+    CREATE_COURSE_MODAL_TITLE: "Course creation",
   },
 };
