c92b53283ef0251ae518561c2c27464fdbae5a26
Santiago Gonzalez Rojas <santiago@ensolvers.com>
2022-02-22 23:57:09 -0300

    Add new generic approach for GraphQL Service

--- src/services/GraphQLService.ts
+++ src/services/GraphQLService.ts
@@ -1,5 +1,17 @@
-import { GraphQLOptions, GraphQLResult } from "@aws-amplify/api-graphql";
+import {
+  graphqlOperation,
+  GraphQLOptions,
+  GraphQLResult,
+} from "@aws-amplify/api-graphql";
 import { API } from "aws-amplify";
+import { GRAPHQL_MAX_PAGE_RESULTS } from "../constants/GraphQL";
+import { LogLevel, LogTypes } from "../enums/LogTypes";
+import Logger from "../utils/Logger";
+
+export interface GraphQLResultWithNextToken<T> {
+  items: T | T[];
+  nextToken: string;
+}
 
 interface SingleResult<T> {
   [key: string]: T | Array<T>;
@@ -23,6 +35,34 @@ class GraphQLService {
       return query;
     } catch (e) {
       console.log(e);
+    }
+  };
+
+  public fetchQuery = async <T>(
+    query: string,
+    nextToken?: string,
+    limit = GRAPHQL_MAX_PAGE_RESULTS
+  ): Promise<GraphQLResultWithNextToken<T> | undefined> => {
+    try {
+      const models = await this.graphQL<any>(
+        graphqlOperation(query, {
+          limit,
+          nextToken,
+        })
+      );
+
+      //TODO: Improve this weird handling to get the results from GraphQL
+      const result = Object.entries(models || {})[0][1];
+      const queryKey = Object.entries(result)[0][0];
+
+      return result[queryKey];
+    } catch (e) {
+      Logger.log(
+        LogLevel.ERROR,
+        LogTypes.GraphQLService,
+        "Error when executing Graph QL Query",
+        e
+      );
     }
   };
 }
