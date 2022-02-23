95bd94e6aab4b73ccea341998a4df22a616163e8
Santiago Gonzalez Rojas <santiago@ensolvers.com>
2022-02-22 23:46:51 -0300

    Renaming components

--- src/enums/Links.ts
+++
@@ -1,6 +0,0 @@
-export enum Links {
-  Website = "https://www.theofficeenglishlearning.com",
-  DevelopedBy = "https://go.santek.dev/me",
-  Facebook = "https://www.facebook.com/theofficeingles",
-  Instagram = "https://www.instagram.com/theoffice.englishlearning/",
-}
---
+++ src/views/sections/students/StudentsHomeScreen.tsx
@@ -0,0 +1,3 @@
+import { Text } from "@chakra-ui/react";
+
+export const StudentsHomeScreen = () => <Text>Holix</Text>;
