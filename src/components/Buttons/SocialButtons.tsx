ca7cba35c9967f002cf08a9d70f063077496aa58
Santiago Gonzalez Rojas <santiago@ensolvers.com>
2022-03-06 23:08:21 -0300

    Add Tooltip to Social Button

--- src/components/Buttons/SocialButtons.tsx
+++ src/components/Buttons/SocialButtons.tsx
@@ -1,45 +1,39 @@
-import { Circle, Stack, useColorModeValue, VisuallyHidden } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import React from 'react'
+import { Circle, Stack, Tag, useColorModeValue, VisuallyHidden } from '@chakra-ui/react'
 import { BsCodeSlash } from 'react-icons/bs'
 import { FaInstagram } from 'react-icons/fa'
 import { FiLink } from 'react-icons/fi'
 import { RiFacebookBoxLine } from 'react-icons/ri'
 import { GeneralInformation } from '../../enums/GeneralInformation'
+import { TooltipHelper } from '../Tooltips/Tooltip'
 
-const SocialButton = ({
-  children,
-  label,
-  href
-}: {
-    children: ReactNode;
-    label: string;
-    href: string;
-  }) => {
-  return (
-      <Circle
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        cursor={'pointer'}
-        as={'a'}
-        href={href}
-        target="_blank"
-        size={8}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200')
-        }}
-      >
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </Circle>
-  )
-}
+// eslint-disable-next-line react/display-name
+const SocialButton = React.forwardRef(({ children, href, label, ...rest }: any, ref) => (
+  <Circle
+    bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+    rounded={'full'}
+    cursor={'pointer'}
+    as={'a'}
+    href={href}
+    target="_blank"
+    size={8}
+    display={'inline-flex'}
+    alignItems={'center'}
+    justifyContent={'center'}
+    transition={'background 0.3s ease'}
+    _hover={{
+      bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200')
+    }}
+  >
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <Tag ref={ref as any} {...rest}>
+      {children}
+    </Tag>
+  </Circle>
+))
 
 export const SocialButtons = () => (
-    <Stack direction={'row'} spacing={6}>
+  <Stack direction={'row'} spacing={6}>
     <SocialButton label={'Website'} href={GeneralInformation.WEBSITE}>
       <FiLink />
     </SocialButton>
@@ -49,11 +43,13 @@ export const SocialButtons = () => (
     <SocialButton label={'Instagram'} href={GeneralInformation.INSTAGRAM}>
       <FaInstagram />
     </SocialButton>
-    <SocialButton
-      label={'Developer'}
-      href={GeneralInformation.DEVELOPED_BY}
-    >
-      <BsCodeSlash />
-    </SocialButton>
+    <TooltipHelper label='Developed with &#10084; by Santek'>
+      <SocialButton
+        label={'Developer'}
+        href={GeneralInformation.DEVELOPED_BY}
+      >
+        <BsCodeSlash />
+      </SocialButton>
+    </TooltipHelper>
   </Stack>
 )
