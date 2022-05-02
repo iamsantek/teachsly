8ca1225fbdb0219bf25c7a00ca93c89f8f1be02d
Santiago Gonzalez Rojas <santiagogonzalezrojas1@gmail.com>
2022-05-02 15:37:18 +0200

    Add permissions to drawer input

--- src/views/media/folders/MediaFolderEditableList.tsx
+++ src/views/media/folders/MediaFolderEditableList.tsx
@@ -1,6 +1,9 @@
 import { List, ListItem } from '@chakra-ui/react'
+import { useContext } from 'react'
 import { EditableInputComponent } from '../../../components/Inputs/EditableInput'
+import { UserDashboardContext } from '../../../contexts/UserDashboardContext'
 import { MediaDrawer } from '../../../interfaces/Media'
+import { isAdmin } from '../../../utils/CognitoGroupsUtils'
 import { getFileTypeIcon } from '../../../utils/MediaUtils'
 
 interface Props {
@@ -10,6 +13,8 @@ interface Props {
 }
 
 export const MediaFolderEditableList = ({ files, onUpdate, onDelete }: Props) => {
+  const { context: { user } } = useContext(UserDashboardContext)
+  const hasAdminRole = !!isAdmin(user)
   if (files.length === 0) {
     return null
   }
@@ -21,16 +26,24 @@ export const MediaFolderEditableList = ({ files, onUpdate, onDelete }: Props) =>
 
   return (
         <List spacing={3}>
-            {files.map((media) => (
+            {files.map((media) => {
+              const canEdit = !!(media.owner === user?.cognitoId || !media.isUploaded || isAdmin(user))
+
+              return (
                 <ListItem key={media.id} display='flex' gap={3} alignItems='flex-start'>
                     {getFileTypeIcon(media.type)}
                     <EditableInputComponent
                     value={media.title}
                     onComplete={(newValue) => onComplete(media, newValue)}
                     onDelete={() => onDelete(media)}
+                    permissions={{
+                      canEdit,
+                      canDelete: hasAdminRole
+                    }}
                     />
                 </ListItem>
-            ))}
+              )
+            })}
         </List>
   )
 }
