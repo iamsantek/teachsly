4e4b86151069021a68470c8c9bde79f5cc26a560
Santek <santiagogonzalezrojas1@gmail.com>
2022-07-15 12:24:04 +0200

    Remove unsued dependencies

--- src/constants/Environment.ts
+++ src/constants/Environment.ts
@@ -1,8 +1,8 @@
 import { GeneralInformation } from '../enums/GeneralInformation'
 import { BackendEnvironments } from '../interfaces/BackendEnvironments'
 
-const API_ENDPOINT = `https://api-qa.${GeneralInformation.DOMAIN}/graphql`
+const API_ENDPOINT = `https://api.${GeneralInformation.DOMAIN}/graphql`
 
 export const GRAPHQL_ENDPOINT = process.env.REACT_APP_GRAPHQL_API_ENDPOINT || API_ENDPOINT
 
-export const BACKEND_ENV = process.env.REACT_APP_ENV || BackendEnvironments.QA
+export const BACKEND_ENV = 'prod' || process.env.REACT_APP_ENV || BackendEnvironments.QA
