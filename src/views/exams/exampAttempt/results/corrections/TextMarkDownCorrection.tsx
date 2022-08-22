5d5e564381195651f36bceab55b1ba18837d7516
= <santiagogonzalezrojas1@gmail.com>
2022-08-22 23:23:40 +0200

    UI improvements for mark down corrections

--- src/views/exams/exampAttempt/results/corrections/TextMarkDownCorrection.tsx
+++ src/views/exams/exampAttempt/results/corrections/TextMarkDownCorrection.tsx
@@ -1,62 +1,47 @@
-import { Text, Mark, Stack, Textarea, useHighlight, Box } from '@chakra-ui/react'
+import { Text, Stack, Textarea, useHighlight, Box } from '@chakra-ui/react'
 import { useState } from 'react'
+import { useFormContext } from 'react-hook-form';
+import { ExamForm } from '../../../../../interfaces/Exams';
+import { generateCorrectionMatches } from '../../../../../utils/ExamUtils';
 import { translate } from '../../../../../utils/LanguageUtils';
 import { MarkDownColorHelper } from './MarkDownColorHelper';
+import { TextMarkdownViewer } from './TextMarkdownViewer';
 
 interface Props {
   text: string;
+  questionPoolIndex: number;
+  questionIndex: number
 }
 
-export const TextMarkDownCorrection = ({ text }: Props) => {
+export const TextMarkDownCorrection = ({ text, questionIndex, questionPoolIndex }: Props) => {
   const [markDownText, setMarkDownText] = useState(text)
-
-  const asteriskWords = markDownText.match(/\*(.*?)\*/g) || []
-  const doubleAsteriskWords = markDownText.match(/#(.*?)#/g) || []
-  const dashWords = markDownText.match(/-([^-]+)-/g) || []
-  const slashWords = markDownText.match(/\/([^/]+)\//g) || []
-  const parenthesisWords = markDownText.match(/\(([^()]+)\)/g) || []
-
-  const matches = [...asteriskWords, ...doubleAsteriskWords, ...dashWords, ...slashWords, ...parenthesisWords]
+  const { register, watch } = useFormContext<ExamForm>()
+  const { matches } = generateCorrectionMatches(markDownText)
 
   const generalChunks = useHighlight({
     text: markDownText,
     query: matches
   })
 
+  console.log(watch())
 
   const existMatches = generalChunks.some(({ match }) => match)
 
-  const getMarkColor = (text: string) => {
-    if (asteriskWords.includes(text)) {
-      return { color: 'purple.500' }
-    } else if (doubleAsteriskWords.includes(text)) {
-      return { color: 'blue.500' }
-    }
-    else if (dashWords.includes(text)) {
-      return { color: 'pink.500' }
-    }
-    else if (slashWords.includes(text)) {
-      return { color: 'orange.500' }
-    }
-    else if (parenthesisWords.includes(text)) {
-      return { color: 'blackAlpha.500' }
-    }
-    return { color: 'blackAlpha.500' }
-  }
-
   return (
     <Stack>
-      <Textarea marginY={5} minHeight='sm' value={markDownText} noOfLines={10} onChange={(e) => setMarkDownText(e.target.value)} rows={4} />
-      <MarkDownColorHelper />
+      <Textarea
+        marginY={5}
+        {...register(`questionPools.${questionPoolIndex}.questions.${questionIndex}.correction.markDownCorrection`)}
+        value={markDownText}
+        onChange={(e) => setMarkDownText(e.target.value)}
+      />
+      <MarkDownColorHelper onResetCorrection={() => setMarkDownText(text)} />
       <Box marginY={3}>
         {existMatches && <Text textStyle='title' marginY={3}>{translate('CORRECTION_PREVIEW')}</Text>}
-        {generalChunks.map(({ text, match }) => {
-          if (!match) return text
-          const { color } = getMarkColor(text)
-          return <Mark bgColor={color} color='whiteAlpha.900'>{text}</Mark>
-        }
-        )}
+        <TextMarkdownViewer markdownText={markDownText} />
       </Box>
     </Stack>
   )
 }
+
+
