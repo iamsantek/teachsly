b176e7146eef773975f18e1689e562a22a31452b
= <santiagogonzalezrojas1@gmail.com>
2022-11-08 19:52:31 +0100

    Fix lesson endless loading state

--- src/views/lessonsPlanning/LessonsPlanningHomeScreen.tsx
+++ src/views/lessonsPlanning/LessonsPlanningHomeScreen.tsx
@@ -1,211 +1,306 @@
-import { LessonsPlanningList } from './LessonsPlanningList'
-import { useCallback, useContext, useEffect, useReducer, useState } from 'react'
-import { deleteLessonPlan, getLessonPlansByCourseId } from '../../services/LessonPlanService'
-import { CreateLessonPlanInput, EnglishLevel, Exam, ExamAttempt, ExamType, LessonPlan, LessonPlanningType } from '../../API'
-import { findAndUpdateContent } from '../../utils/GeneralUtils'
-import MediaService from '../../services/MediaService'
-import { useParams } from 'react-router-dom'
-import { UserDashboardContext } from '../../contexts/UserDashboardContext'
-import ExamService from '../../services/ExamService'
-import { LessonPlanningCourseInformation } from './LessonPlanningCourseInformation'
-import { Placeholder } from '../../components/Placeholders/Placeholder'
-import { ContentLinePlaceholder } from '../../components/Placeholders/ContentLinePlaceholder'
-import { LessonPlanningItem } from '../../interfaces/LessonPlanning'
-import { Badge, Box } from '@chakra-ui/react'
-import { getBadgeWithExamAttempt } from '../../utils/ExamUtils'
-import { useUserGroups } from '../../hooks/useUserGroups'
-import { NoContentPlaceholder } from '../../components/Placeholders/NoContentPlaceholder'
+import { LessonsPlanningList } from "./LessonsPlanningList";
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import {
+  deleteLessonPlan,
+  getLessonPlansByCourseId,
+} from "../../services/LessonPlanService";
+import {
+  CreateLessonPlanInput,
+  EnglishLevel,
+  Exam,
+  ExamAttempt,
+  ExamType,
+  LessonPlan,
+  LessonPlanningType,
+} from "../../API";
+import { findAndUpdateContent } from "../../utils/GeneralUtils";
+import MediaService from "../../services/MediaService";
+import { useParams } from "react-router-dom";
+import { UserDashboardContext } from "../../contexts/UserDashboardContext";
+import ExamService from "../../services/ExamService";
+import { LessonPlanningCourseInformation } from "./LessonPlanningCourseInformation";
+import { Placeholder } from "../../components/Placeholders/Placeholder";
+import { ContentLinePlaceholder } from "../../components/Placeholders/ContentLinePlaceholder";
+import { LessonPlanningItem } from "../../interfaces/LessonPlanning";
+import { Badge, Box } from "@chakra-ui/react";
+import { getBadgeWithExamAttempt } from "../../utils/ExamUtils";
+import { useUserGroups } from "../../hooks/useUserGroups";
+import { NoContentPlaceholder } from "../../components/Placeholders/NoContentPlaceholder";
 
 enum FetchType {
-  EXAMS = 'EXAMS',
-  MEDIA = 'MEDIA',
-  EXAM_ATTEMPTS = 'EXAM_ATTEMPTS',
-  LESSONS = 'LESSONS'
+  EXAMS = "EXAMS",
+  MEDIA = "MEDIA",
+  EXAM_ATTEMPTS = "EXAM_ATTEMPTS",
+  LESSONS = "LESSONS",
 }
 
 const initialNextPageTokens: { [key in FetchType]: string | undefined } = {
   [FetchType.EXAMS]: undefined,
   [FetchType.EXAM_ATTEMPTS]: undefined,
   [FetchType.MEDIA]: undefined,
-  [FetchType.LESSONS]: undefined
-}
+  [FetchType.LESSONS]: undefined,
+};
 
-const nextPageTokenReducer = (state: { [key in FetchType]: string | undefined }, action: { type: FetchType, payload: string | undefined | null }) => {
+const nextPageTokenReducer = (
+  state: { [key in FetchType]: string | undefined },
+  action: { type: FetchType; payload: string | undefined | null }
+) => {
   return {
     ...state,
-    [action.type]: action.payload
-  }
-}
+    [action.type]: action.payload,
+  };
+};
 
 export const LessonsPlanningHomeScreen = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [lessons, setLessons] = useState<LessonPlanningItem[]>([])
-  const [mediaLoading, setMediaLoading] = useState(true)
-  const [examLoading, setExamLoading] = useState(true)
-  const [examAttemptsLoading, setExamAttemptsLoading] = useState(true)
-  const [examAttempts, setExamAttempts] = useState<ExamAttempt[]>([])
-  const [lessonPlanningLoading, setLessonPlanningLoading] = useState(true)
-  const [lessonToUpdate, setLessonToUpdate] = useState<LessonPlan | null>(null)
-  const [exams, setExams] = useState<Exam[]>([])
-  const { courseId } = useParams()
-  const { context: { user } } = useContext(UserDashboardContext)
-  const [nextPageTokens, dispatch] = useReducer(nextPageTokenReducer, initialNextPageTokens)
-  const { hasAdminRole, hasStudentRole } = useUserGroups()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [lessons, setLessons] = useState<LessonPlanningItem[]>([]);
+  const [mediaLoading, setMediaLoading] = useState(true);
+  const [examLoading, setExamLoading] = useState(true);
+  const [examAttemptsLoading, setExamAttemptsLoading] = useState(true);
+  const [examAttempts, setExamAttempts] = useState<ExamAttempt[]>([]);
+  const [lessonPlanningLoading, setLessonPlanningLoading] = useState(true);
+  const [lessonToUpdate, setLessonToUpdate] = useState<LessonPlan | null>(null);
+  const [exams, setExams] = useState<Exam[]>([]);
+  const { courseId } = useParams();
+  const {
+    context: { user },
+  } = useContext(UserDashboardContext);
+  const [nextPageTokens, dispatch] = useReducer(
+    nextPageTokenReducer,
+    initialNextPageTokens
+  );
+  const { hasAdminRole, hasStudentRole } = useUserGroups();
 
   const fetchLessonPlans = useCallback(async () => {
-    setLessonPlanningLoading(true)
-    const lessonPlans = await getLessonPlansByCourseId(courseId as string, nextPageTokens.LESSONS)
+    setLessonPlanningLoading(true);
+    const lessonPlans = await getLessonPlansByCourseId(
+      courseId as string,
+      nextPageTokens.LESSONS
+    );
     if (lessonPlans?.listLessonPlans?.items) {
-      setLessons(currentLessons => currentLessons.concat(lessonPlans.listLessonPlans?.items as LessonPlan[]))
+      setLessons((currentLessons) =>
+        currentLessons.concat(
+          lessonPlans.listLessonPlans?.items as LessonPlan[]
+        )
+      );
     }
 
     if (lessonPlans?.listLessonPlans?.nextToken) {
-      dispatch({ type: FetchType.LESSONS, payload: lessonPlans.listLessonPlans.nextToken })
+      dispatch({
+        type: FetchType.LESSONS,
+        payload: lessonPlans.listLessonPlans.nextToken,
+      });
     }
 
-    setLessonPlanningLoading(false)
-  }, [nextPageTokens.LESSONS, courseId])
+    setLessonPlanningLoading(false);
+  }, [nextPageTokens.LESSONS, courseId]);
 
   const generateLessonPlanningByExams = useCallback(() => {
-    const lessonPlannings: CreateLessonPlanInput[] = exams?.map(exam => {
-      const examAttempt = examAttempts.find(attempt => attempt.examName === exam?.title && attempt.score)
-      const pendingCorrection = examAttempt?.isCompleted && !examAttempt.correctedBy
-      const { badgeColor, badgeText } = getBadgeWithExamAttempt(exam, examAttempt as ExamAttempt)
+    const lessonPlannings: CreateLessonPlanInput[] = exams
+      ?.map((exam) => {
+        const examAttempt = examAttempts.find(
+          (attempt) => attempt.examName === exam?.title && attempt.score
+        );
+        const pendingCorrection =
+          examAttempt?.isCompleted && !examAttempt.correctedBy;
+        const { badgeColor, badgeText } = getBadgeWithExamAttempt(
+          exam,
+          examAttempt as ExamAttempt
+        );
 
-      return {
-        id: exam?.id,
-        title: exam?.title,
-        type: exam?.type === ExamType.HOMEWORK ? LessonPlanningType.HOMEWORK : LessonPlanningType.EXAM,
-        date: exam?.createdAt,
-        renderElement: hasStudentRole && (
-          <Box marginX={10}>
-            <Badge colorScheme={badgeColor}>
-              {badgeText}
-            </Badge>
-          </Box>
-        ),
-        examAttemptId: examAttempt?.id,
-        pendingCorrection,
-        isCompleted: examAttempt?.isCompleted,
-      }
-    }).filter(x => x) as LessonPlanningItem[]
+        return {
+          id: exam?.id,
+          title: exam?.title,
+          type:
+            exam?.type === ExamType.HOMEWORK
+              ? LessonPlanningType.HOMEWORK
+              : LessonPlanningType.EXAM,
+          date: exam?.createdAt,
+          renderElement: hasStudentRole && (
+            <Box marginX={10}>
+              <Badge colorScheme={badgeColor}>{badgeText}</Badge>
+            </Box>
+          ),
+          examAttemptId: examAttempt?.id,
+          pendingCorrection,
+          isCompleted: examAttempt?.isCompleted,
+        };
+      })
+      .filter((x) => x) as LessonPlanningItem[];
 
-    setLessons(currentLessons => currentLessons.concat(lessonPlannings as LessonPlanningItem[]))
-  }, [examAttempts, exams, hasStudentRole])
+    setLessons((currentLessons) =>
+      currentLessons.concat(lessonPlannings as LessonPlanningItem[])
+    );
+  }, [examAttempts, exams, hasStudentRole]);
 
   const fetchExamAttempts = useCallback(async () => {
     if (exams.length === 0) {
-      return
+      if (examAttemptsLoading) {
+        setExamAttemptsLoading(false);
+      }
+      return;
     }
 
-    const examNames = exams.map(exam => exam.title as string)
-    const examAttempts = await ExamService.getExamAttemptsByExamNameAndUserId(examNames, user?.cognitoId as string, nextPageTokens.EXAM_ATTEMPTS)
+    const examNames = exams.map((exam) => exam.title as string);
+    const examAttempts = await ExamService.getExamAttemptsByExamNameAndUserId(
+      examNames,
+      user?.cognitoId as string,
+      nextPageTokens.EXAM_ATTEMPTS
+    );
 
     if ((examAttempts?.listExamAttempts?.items?.length ?? 0) > 0) {
-      setExamAttempts(currentExamAttempts => currentExamAttempts.concat(examAttempts?.listExamAttempts?.items as ExamAttempt[]))
+      setExamAttempts((currentExamAttempts) =>
+        currentExamAttempts.concat(
+          examAttempts?.listExamAttempts?.items as ExamAttempt[]
+        )
+      );
     }
-    if (examAttempts?.listExamAttempts?.nextToken && examAttempts.listExamAttempts.items.length !== 0) {
-      dispatch({ type: FetchType.EXAM_ATTEMPTS, payload: examAttempts.listExamAttempts.nextToken })
+    if (
+      examAttempts?.listExamAttempts?.nextToken &&
+      examAttempts.listExamAttempts.items.length !== 0
+    ) {
+      console.log("next token", examAttempts?.listExamAttempts?.nextToken);
+      console.log("Items", examAttempts.listExamAttempts.items);
+      dispatch({
+        type: FetchType.EXAM_ATTEMPTS,
+        payload: examAttempts.listExamAttempts.nextToken,
+      });
     } else {
-      setExamAttemptsLoading(false)
-      generateLessonPlanningByExams()
+      setExamAttemptsLoading(false);
+      generateLessonPlanningByExams();
     }
-  }, [nextPageTokens.EXAM_ATTEMPTS, user?.cognitoId, exams, generateLessonPlanningByExams])
+  }, [
+    nextPageTokens.EXAM_ATTEMPTS,
+    user?.cognitoId,
+    exams,
+    generateLessonPlanningByExams,
+    examAttemptsLoading,
+  ]);
 
   const fetchExams = useCallback(async () => {
-    const exams = await ExamService.fetchExamsByCourseId(courseId, user?.englishLevel as EnglishLevel, nextPageTokens.EXAMS)
+    const exams = await ExamService.fetchExamsByCourseId(
+      courseId,
+      user?.englishLevel as EnglishLevel,
+      nextPageTokens.EXAMS
+    );
 
     if ((exams?.listExams?.items?.length ?? 0) > 0) {
-      setExams(currentExams => currentExams.concat(exams?.listExams?.items as Exam[]))
+      setExams((currentExams) =>
+        currentExams.concat(exams?.listExams?.items as Exam[])
+      );
     }
 
     if (exams?.listExams?.nextToken && exams.listExams.items.length !== 0) {
-      dispatch({ type: FetchType.EXAMS, payload: exams.listExams.nextToken })
+      dispatch({ type: FetchType.EXAMS, payload: exams.listExams.nextToken });
     } else {
-      setExamLoading(false)
+      setExamLoading(false);
     }
-  }, [nextPageTokens.EXAMS, courseId, user?.englishLevel])
+  }, [nextPageTokens.EXAMS, courseId, user?.englishLevel]);
 
   useEffect(() => {
     if (!examLoading) {
-      fetchExamAttempts()
+      fetchExamAttempts();
     }
-  }, [examLoading, fetchExamAttempts])
+  }, [examLoading, fetchExamAttempts]);
 
   const fetchMedias = useCallback(async () => {
-    const medias = await MediaService.fetchMediaByCourseId(courseId, user?.englishLevel as EnglishLevel, false, nextPageTokens.MEDIA)
+    const medias = await MediaService.fetchMediaByCourseId(
+      courseId,
+      user?.englishLevel as EnglishLevel,
+      false,
+      nextPageTokens.MEDIA
+    );
 
+    const newLessonPlans: CreateLessonPlanInput[] = medias?.listMedia?.items
+      ?.map((media) => {
+        return {
+          id: media?.id,
+          title: media?.title,
+          content: media?.description,
+          uploadedBy: media?.uploadedBy,
+          type: LessonPlanningType.MEDIA,
+          date: media?.createdAt,
+          media: media?.link,
+        };
+      })
+      .filter((x) => x) as CreateLessonPlanInput[];
 
-    const newLessonPlans: CreateLessonPlanInput[] = medias?.listMedia?.items?.map(media => {
-      return {
-        id: media?.id,
-        title: media?.title,
-        content: media?.description,
-        uploadedBy: media?.uploadedBy,
-        type: LessonPlanningType.MEDIA,
-        date: media?.createdAt,
-        media: media?.link
-      }
-    }).filter(x => x) as CreateLessonPlanInput[]
-
-    setLessons(lessonPlans => lessonPlans.concat(newLessonPlans as LessonPlanningItem[]))
+    setLessons((lessonPlans) =>
+      lessonPlans.concat(newLessonPlans as LessonPlanningItem[])
+    );
 
     if (medias?.listMedia?.nextToken) {
-      dispatch({ type: FetchType.MEDIA, payload: medias.listMedia.nextToken })
+      dispatch({ type: FetchType.MEDIA, payload: medias.listMedia.nextToken });
     }
 
-    setMediaLoading(false)
-  }, [nextPageTokens.MEDIA, courseId, user?.englishLevel])
+    setMediaLoading(false);
+  }, [nextPageTokens.MEDIA, courseId, user?.englishLevel]);
 
   useEffect(() => {
-    const isAllowedCourse = user?.groups?.some(group => group === courseId)
+    const isAllowedCourse = user?.groups?.some((group) => group === courseId);
 
     if (!isAllowedCourse && !hasAdminRole) {
-      window.location.href = '/'
-      return
+      window.location.href = "/";
+      return;
     }
-  }, [user?.groups, courseId, hasAdminRole])
+  }, [user?.groups, courseId, hasAdminRole]);
 
   useEffect(() => {
-    fetchMedias()
-  }, [fetchMedias])
+    fetchMedias();
+  }, [fetchMedias]);
 
   useEffect(() => {
-    fetchLessonPlans()
-  }, [fetchLessonPlans])
+    fetchLessonPlans();
+  }, [fetchLessonPlans]);
 
   useEffect(() => {
-    fetchExams()
-  }, [fetchExams])
+    fetchExams();
+  }, [fetchExams]);
 
   // Sort elements by date (asc)
   const sortLessons = (lessons: LessonPlan[]) => {
     return lessons.sort((a, b) => {
-      const dateA: Date = new Date(a.date)
-      const dateB: Date = new Date(b.date)
-      return dateA.getTime() - dateB.getTime()
-    })
-  }
+      const dateA: Date = new Date(a.date);
+      const dateB: Date = new Date(b.date);
+      return dateA.getTime() - dateB.getTime();
+    });
+  };
 
   const onDelete = async (lessonPlan: LessonPlan) => {
-    const deletedLessonPlan = await deleteLessonPlan(lessonPlan.id)
+    const deletedLessonPlan = await deleteLessonPlan(lessonPlan.id);
     if (deletedLessonPlan) {
-      const newLessons = lessons.filter(lesson => lesson.id !== lessonPlan.id)
-      setLessons(newLessons)
+      const newLessons = lessons.filter(
+        (lesson) => lesson.id !== lessonPlan.id
+      );
+      setLessons(newLessons);
     }
-  }
+  };
 
   const onUpdate = (lessonPlan: LessonPlan) => {
-    setLessonToUpdate(lessonPlan)
-    setIsModalOpen(true)
-  }
+    setLessonToUpdate(lessonPlan);
+    setIsModalOpen(true);
+  };
 
   const onUpdateSuccess = (lessonPlan: LessonPlanningItem) => {
-    const newLessons = findAndUpdateContent(lessonPlan, lessons)
-    setLessons(newLessons)
-  }
+    const newLessons = findAndUpdateContent(lessonPlan, lessons);
+    setLessons(newLessons);
+  };
 
-  const isLoading = mediaLoading || examLoading || lessonPlanningLoading || examAttemptsLoading
+  const isLoading =
+    mediaLoading || examLoading || lessonPlanningLoading || examAttemptsLoading;
+
+  //Console log all the loading states
+  console.log("mediaLoading", mediaLoading);
+  console.log("examLoading", examLoading);
+  console.log("lessonPlanningLoading", lessonPlanningLoading);
+  console.log("examAttemptsLoading", examAttemptsLoading);
+  console.log("----");
 
   return (
     <>
@@ -217,28 +312,25 @@ export const LessonsPlanningHomeScreen = () => {
         number={10}
         placeholderElement={<ContentLinePlaceholder />}
       />
-      <NoContentPlaceholder
-        show={!isLoading && lessons.length === 0}
-      />
+      <NoContentPlaceholder show={!isLoading && lessons.length === 0} />
       {!isLoading && lessons.length !== 0 && (
         <LessonsPlanningList
           onCreate={(lessonPlan) => {
-            const sortedLessons = sortLessons([...lessons, lessonPlan])
-            setLessons(sortedLessons)
+            const sortedLessons = sortLessons([...lessons, lessonPlan]);
+            setLessons(sortedLessons);
           }}
           onDelete={onDelete}
           onUpdate={onUpdate}
           lessons={sortLessons(lessons)}
           isModalOpen={isModalOpen}
           onCloseModal={() => {
-            setIsModalOpen(false)
-            setLessonToUpdate(null)
+            setIsModalOpen(false);
+            setLessonToUpdate(null);
           }}
           lessonToUpdate={lessonToUpdate}
           onUpdateSuccess={onUpdateSuccess}
         />
       )}
     </>
-  )
-}
-
+  );
+};
--- src/views/lessonsPlanning/LessonsPlanningList.tsx
+++ src/views/lessonsPlanning/LessonsPlanningList.tsx
@@ -1,87 +1,108 @@
-import { Badge, Box, Flex, Stack, Text } from "@chakra-ui/react"
-import { ExamType, LessonPlan, LessonPlanningType } from "../../API"
-import { ContentLine } from "../../components/ContentLine/ContentLine"
-import { contentLineColor } from "../../constants/LessonPlanning"
-import { useUserGroups } from "../../hooks/useUserGroups"
-import { LessonPlanningItem } from "../../interfaces/LessonPlanning"
-import StorageService from "../../services/aws/StorageService"
-import { translate } from "../../utils/LanguageUtils"
-import { CommonContentLineTitle } from "../media/CommonContentLineTitle"
-import { LessonPlanningModal } from "./LessonPlanningModal"
+import { Badge, Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { ExamType, LessonPlan, LessonPlanningType } from "../../API";
+import { ContentLine } from "../../components/ContentLine/ContentLine";
+import { contentLineColor } from "../../constants/LessonPlanning";
+import { useUserGroups } from "../../hooks/useUserGroups";
+import { LessonPlanningItem } from "../../interfaces/LessonPlanning";
+import StorageService from "../../services/aws/StorageService";
+import { translate } from "../../utils/LanguageUtils";
+import { CommonContentLineTitle } from "../media/CommonContentLineTitle";
+import { LessonPlanningModal } from "./LessonPlanningModal";
 
 interface Props {
-  lessons: LessonPlanningItem[]
-  onCreate: (lessonPlan: LessonPlan) => void
-  onDelete: (lessonPlan: LessonPlan) => void
-  onUpdate: (lessonPlan: LessonPlan) => void
+  lessons: LessonPlanningItem[];
+  onCreate: (lessonPlan: LessonPlan) => void;
+  onDelete: (lessonPlan: LessonPlan) => void;
+  onUpdate: (lessonPlan: LessonPlan) => void;
   isModalOpen: boolean;
   onCloseModal: () => void;
   lessonToUpdate: LessonPlan | null;
   onUpdateSuccess: (lessonPlan: LessonPlan) => void;
 }
 
-export const LessonsPlanningList = ({ lessons, onCreate, onDelete, onUpdate, isModalOpen, onCloseModal, lessonToUpdate, onUpdateSuccess }: Props) => {
-  const { hasEditPermission } = useUserGroups()
+export const LessonsPlanningList = ({
+  lessons,
+  onCreate,
+  onDelete,
+  onUpdate,
+  isModalOpen,
+  onCloseModal,
+  lessonToUpdate,
+  onUpdateSuccess,
+}: Props) => {
+  const { hasEditPermission } = useUserGroups();
 
   // Format date like this: 30 July
   const formatDate = (date: string) => {
-    const dateObj = new Date(date)
-    const month = dateObj.toLocaleString('default', { month: 'short' })
-    const day = dateObj.getDate()
-    return { day, month }
-  }
+    const dateObj = new Date(date);
+    const month = dateObj.toLocaleString("default", { month: "short" });
+    const day = dateObj.getDate();
+    return { day, month };
+  };
 
   const LessonDate = ({ date }: { date: string }) => {
-    const { day, month } = formatDate(date)
-    return <Flex flexDirection='column' zIndex={11}>
-      <Text fontSize={10} fontWeight='bold' textAlign={'center'}> {day}</Text >
-      <Text fontSize={10} fontWeight='bold' textAlign={'center'}> {month}</Text >
-    </Flex>
-  }
+    const { day, month } = formatDate(date);
+    return (
+      <Flex flexDirection="column" zIndex={11}>
+        <Text fontSize={10} fontWeight="bold" textAlign={"center"}>
+          {" "}
+          {day}
+        </Text>
+        <Text fontSize={10} fontWeight="bold" textAlign={"center"}>
+          {" "}
+          {month}
+        </Text>
+      </Flex>
+    );
+  };
 
   const onViewExamHandler = (lesson: LessonPlanningItem, type: ExamType) => {
-    const examType = type === ExamType.EXAM ? 'exams' : 'homework'
+    const examType = type === ExamType.EXAM ? "exams" : "homework";
 
     if (!lesson.isCompleted) {
-      window.open(`/${examType}/${lesson?.id}/intro`, '_blank')
-      return
+      window.open(`/${examType}/${lesson?.id}/intro`, "_blank");
+      return;
     }
 
     if (!lesson.pendingCorrection) {
-      window.open(`/${examType}/results/${lesson.examAttemptId}`, '_blank')
-      return
+      window.open(`/${examType}/results/${lesson.examAttemptId}`, "_blank");
+      return;
     }
 
     if (hasEditPermission) {
-      window.open(`/${examType}/attempt/${lesson.examAttemptId}`, '_blank')
+      window.open(`/${examType}/attempt/${lesson.examAttemptId}`, "_blank");
     } else {
-      return null
+      return null;
     }
-  }
+  };
 
   const onView = async (lesson: LessonPlanningItem) => {
     switch (lesson.type) {
       case LessonPlanningType.MEDIA:
         // Open in a new tab the media
-        const mediaLink = await StorageService.getSignedUrl(lesson.media as string)
-        window.open(mediaLink?.url, '_blank')
-        break
+        const mediaLink = await StorageService.getSignedUrl(
+          lesson.media as string
+        );
+        window.open(mediaLink?.url, "_blank");
+        break;
       case LessonPlanningType.HOMEWORK:
-        onViewExamHandler(lesson, ExamType.HOMEWORK)
-        break
+        onViewExamHandler(lesson, ExamType.HOMEWORK);
+        break;
       case LessonPlanningType.EXAM:
-        onViewExamHandler(lesson, ExamType.EXAM)
-        break
+        onViewExamHandler(lesson, ExamType.EXAM);
+        break;
       case LessonPlanningType.LESSON:
         if (lesson.media) {
-          const mediaLink = await StorageService.getSignedUrl(lesson.media as string)
-          window.open(mediaLink?.url, '_blank')
+          const mediaLink = await StorageService.getSignedUrl(
+            lesson.media as string
+          );
+          window.open(mediaLink?.url, "_blank");
         }
-        break
+        break;
       default:
-        return undefined
+        return undefined;
     }
-  }
+  };
 
   return (
     <>
@@ -93,8 +114,10 @@ export const LessonsPlanningList = ({ lessons, onCreate, onDelete, onUpdate, isM
         lessonToUpdate={lessonToUpdate}
       />
       <Stack spacing={5} marginBottom={10}>
-        <Text textStyle='title' fontSize='xl'>{translate('LESSON_PLAN_TITLE')}</Text>
-        <Box position={'relative'}>
+        <Text textStyle="title" fontSize="xl">
+          {translate("LESSON_PLAN_TITLE")}
+        </Text>
+        <Box position={"relative"}>
           <Box
             marginLeft={8}
             position={"absolute"}
@@ -105,8 +128,8 @@ export const LessonsPlanningList = ({ lessons, onCreate, onDelete, onUpdate, isM
             zIndex={1}
           />
           <Stack spacing={5}>
-            {lessons.map(lesson => {
-              const isLessonPlan = lesson.type === LessonPlanningType.LESSON
+            {lessons.map((lesson) => {
+              const isLessonPlan = lesson.type === LessonPlanningType.LESSON;
               return (
                 <Box>
                   <ContentLine
@@ -114,30 +137,47 @@ export const LessonsPlanningList = ({ lessons, onCreate, onDelete, onUpdate, isM
                     transformOnHover={false}
                     leftIcon={<LessonDate date={lesson.date} />}
                     noBorder={true}
-                    onDelete={hasEditPermission && isLessonPlan ? () => onDelete(lesson) : undefined}
-                    onEdit={hasEditPermission && isLessonPlan ? () => onUpdate(lesson) : undefined}
+                    onDelete={
+                      hasEditPermission && isLessonPlan
+                        ? () => onDelete(lesson)
+                        : undefined
+                    }
+                    onEdit={
+                      hasEditPermission && isLessonPlan
+                        ? () => onUpdate(lesson)
+                        : undefined
+                    }
                     onView={() => onView(lesson)}
-
                   >
                     <CommonContentLineTitle
                       id={lesson.id}
                       title={lesson.title}
-                      header={<Badge variant='subtle' color='gray.900' colorScheme={contentLineColor[lesson.type ?? LessonPlanningType.OTHER]} >{lesson.type}</Badge>}
+                      header={
+                        <Badge
+                          variant="subtle"
+                          color="gray.900"
+                          colorScheme={
+                            contentLineColor[
+                              lesson.type ?? LessonPlanningType.OTHER
+                            ]
+                          }
+                        >
+                          {lesson.type}
+                        </Badge>
+                      }
                     >
                       <Text color="gray.600" noOfLines={3}>
                         {lesson.content}
                       </Text>
-                      <Flex gap={3}>
-                        {lesson.renderElement}
-                      </Flex>
+                      <Flex gap={3}>{lesson.renderElement}</Flex>
                     </CommonContentLineTitle>
                   </ContentLine>
                 </Box>
-              )
+              );
             })}
           </Stack>
-        </Box >
+        </Box>
       </Stack>
     </>
-  )
-}
+  );
+};
